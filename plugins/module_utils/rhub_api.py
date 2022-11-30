324fdb716c6e1cc700b0f6f7fe9122dc63d368ba
Filip Pobořil <fpoboril@redhat.com>
2022-11-30 11:41:10 +0100

    Token auth (#6)

--- plugins/module_utils/rhub_api.py
+++ plugins/module_utils/rhub_api.py
@@ -1,5 +1,3 @@
-import datetime
-
 import requests
 
 
@@ -21,36 +19,15 @@ class RHubApiError(Exception):
                    response=response, problem=problem)
 
 
-class RHubAuthError(RHubApiError):
-    pass
-
-
 class RHubApiClient:
-    def __init__(self, addr, username, password):
+    def __init__(self, addr, token):
         self.addr = addr.rstrip('/')
-        self.username = username
-        self.password = password
-        self._auth_token = None
-
-    def get_token(self):
-        now = datetime.datetime.now()
-        if not self._auth_token or self._auth_token['expires'] < now:
-            response = requests.post(
-                f'{self.addr}/v0/auth/token/create',
-                auth=(self.username, self.password),
-            )
-            if not response.ok:
-                raise RHubAuthError.from_requests_response(response)
-            self._auth_token = response.json()
-            self._auth_token['expires'] = now + datetime.timedelta(
-                minutes=self._auth_token['expires_in']
-            )
-        return self._auth_token['access_token']
+        self.token = token
 
     def request(self, method, path, *args, **kwargs):
         kwargs.setdefault('headers', {})
         kwargs['headers'].setdefault('Content-Type', 'application/json')
-        kwargs['headers']['Authorization'] = 'Bearer ' + self.get_token()
+        kwargs['auth'] = ('__token__', self.token)
         response = requests.request(method, f'{self.addr}{path}', *args, **kwargs)
         if not response.ok:
             raise RHubApiError.from_requests_response(response)
